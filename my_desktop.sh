9527c508b2db8340d13d8d5dbc2d2a85626fd658
Dom Cimafranca <domu@cimafranca.com>
2025-12-02 07:55:51 +0800

    [Script] Modified launcher script to also start notes.py

--- my_desktop.sh
+++ my_desktop.sh
@@ -5,6 +5,7 @@ tmux new-session -d -s $SESSION
 tmux split-window -h -t $SESSION -c /home/dom/Projects/newsfeed
 tmux split-window -v -l 3  -t $SESSION:0.1 -c /home/dom/Projects/newsfeed
 
+tmux send-keys -t $SESSION:0.0 'uv run notes.py' C-m
 tmux send-keys -t $SESSION:0.1 'uv run rss_reader.py' C-m
 tmux send-keys -t $SESSION:0.2 'uv run pomodoro.py' C-m
 
